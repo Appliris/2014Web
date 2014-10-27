451686013a647b97c98abb42611007170a7d520b
GaiTakano <sowelu.and.lagu@gmail.com>
2014-10-27 11:06:54 +0900

    make post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141026070801) do
+ActiveRecord::Schema.define(version: 20141026153626) do
+
+  create_table "posts", force: true do |t|
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "name"
