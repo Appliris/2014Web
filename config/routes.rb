25b26087463723cc874e05ae294e3203a928d4ce
Ryoma ABE <arayahomuhomu@gmail.com>
2014-10-29 02:27:14 +0900

    3日分のタイムスケジュール打ち込んだ

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-get 'timetable/timetable'
-root 'timetable#timetable'
+get 'timetable/first'
+root 'timetable#first'
+get 'timetable/second'
+get 'timetable/third'
 # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
