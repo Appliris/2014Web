bb2a28bfb26976851f568e57f1cd1333e49d51b0
Ryoma ABE <arayahomuhomu@gmail.com>
2014-10-31 06:05:53 +0900

    ルーティングをsns画面に変更

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 get 'timetable/first'
-root 'timetable#first'
+
 get 'timetable/second'
 get 'timetable/third'
 # You can have the root of your site routed with "root"
