4de55537a642d077c437e5afcb15059145b25e37
GaiTakano <sowelu.and.lagu@gmail.com>
2014-10-30 09:49:39 +0900

    error　sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,15 @@ class User < ActiveRecord::Base
   
   has_many :posts
   validates :username, presence: true, uniqueness: true
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      #認証の条件式を変更する
+      where(conditions).where(["username = :value", { :value => username }]).first
+    else
+      where(conditions).first
+    end
+  end
   def email_required?
     false
   end
