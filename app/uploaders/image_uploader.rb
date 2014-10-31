eb39a742216cc89495b7f3678e9e65ef232d342a
Ryoma ABE <arayahomuhomu@gmail.com>
2014-11-01 01:47:38 +0900

    fixed typo

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,9 +3,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
-  include Cloudinary::CarrrierWave
+include Cloudinary::CarrierWave
   # include CarrierWave::MiniMagick
+process :convert=> 'png'
+process :tags=> ['post_image']
 
   # Choose what kind of storage to use for this uploader:
 #  storage :file
@@ -36,19 +37,18 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [150, 150]
+    resize_to_fit(100,100)
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
+ # def extension_white_list
+ #   %w(jpg jpeg gif png)
+ # end
   
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
