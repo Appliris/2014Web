cd4bd65bb717b3f5b4dd1a6460601e73246c2fc0
GaiTakano <sowelu.and.lagu@gmail.com>
2014-11-01 02:03:59 +0900

    error sign up in master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030011342) do
+ActiveRecord::Schema.define(version: 20141030161700) do
 
   create_table "posts", force: true do |t|
     t.text     "body"
@@ -94,7 +94,6 @@ ActiveRecord::Schema.define(version: 20141030011342) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["username"], name: "index_users_on_username", unique: true
 
